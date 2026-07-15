0da80b756f8fa9301ca19e0726e386ce081264ba
lugonapa-git <lugonapa@gemail.com>
2026-07-15 18:48:22 -0300

    Configura GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+
+  basePath: isGithubPages ? "/curso3-movielab-app" : "",
+  assetPrefix: isGithubPages ? "/curso3-movielab-app/" : "",
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
